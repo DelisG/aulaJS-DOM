b8aa00f5eb97488fb48795b14cfb068095314a64
DelisG <delisemarciano@gmail.com>
2023-06-16 16:04:46 -0300

    :recycle: Refatorando Codigo adicionando mais funcionalidades á Lampada

--- projeto-lampada/lampada.js
+++ projeto-lampada/lampada.js
@@ -2,24 +2,24 @@ const turnOn = document.getElementById('turnOn');
 const turnOff = document.getElementById('turnOff');
 const lamp = document.getElementById('lamp');
 
-function isLampBroken () {
-    return lamp.src.indexOf('quebrada') > -1;
+function isLampBroken() {
+return lamp.src.indexOf('quebrada') > -1
 }
 
-function lampBroken () {
-    return src="./img/quebrada.jpg";
+function lampOn() {
+if (!isLampBroken()) {
+    lamp.src = './img/ligada.jpg';
+}
 }
 
-function lampOn () {
-    if (!isLampBroken ()) {
-        lamp.src="./img/ligada.jpg";
-    }
+function lampOff() {
+if (!isLampBroken()) {
+    lamp.src = './img/desligada.jpg';
+}
 }
 
-function lampOff () {
-    if (!isLampBroken ()) {
-        lamp.src="./img/desligada.jpg";
-    }
+function lampBroken() {
+return lamp.src= './img/quebrada.jpg';
 }
 
 turnOn.addEventListener('click', lampOn);
@@ -27,38 +27,3 @@ turnOff.addEventListener('click', lampOff);
 lamp.addEventListener('mouseover', lampOn);
 lamp.addEventListener('mouseleave', lampOff);
 lamp.addEventListener('dblclick', lampBroken);
-
-
-// const turnOn = document.getElementById(' turnOn ');
-// const turnOff = document.getElementById(' turnOff ');
-// const lamp = document.getElementById(' lamp ');
-
-// function isLampBroken () {
-//     return lamp.src = "./img/quebrada.jpg";
-// }
-
-// function lampBroken () {
-//   return lamp.src.indexOf("quebrada") > -1;
-// }
-// function lampOn () {
-//   if (!isLampBroken ()) {
-//     lamp.src = "./img/ligada.jpg";
-//   }
-// }
-
-// function lampOff () {
-//   if (!isLampBroken ()) {
-//     lamp.src = "./img/desligada.jpg";
-//   }
-// }
-
-
-
-
-// // função para criar eventos onde o 'primeiro' parametro é o evento e o segundo parametro é o nome da função
-// turnOn.addEventListener('click', lampOn);
-// turnOff.addEventListener('click', lampOff);
-// lamp.addEventListener('mouseover', lampOn);
-// lamp.addEventListener('mouseleave', lampOff);
-// lamp.addEventListener('dblclick', lampBroken);
-
